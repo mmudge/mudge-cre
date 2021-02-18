3882ddfd84e90defaaa8f2a249dae034b2c61418
michael mudge <michaeltmudge@gmail.com>
2021-02-18 10:58:10 -0800

    added factory boy

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,4 @@
 class ListingsController < ApplicationController
-  # before_action :authenticate_user!
-  # before_action :set_listing, only: %i[ show edit update destroy ]
   before_action :set_listing, only: [:show]
 
   def index
@@ -10,13 +8,10 @@ class ListingsController < ApplicationController
   def show
   end
 
+
   private
 
   def set_listing
     @listing = Listing.find(params[:id])
   end
-
-  # def listing_params
-  #   params.require(:listing).permit(:address)
-  # end
 end
--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe ListingsController, type: :controller do
+  before :each do
+    @listing = create(:listing)
+  end
+
   describe "GET Listings#index" do
     it "returns a successfull response" do
+      puts Listing.all.count
       get :index
       assert_response :success
+
+      puts @response.inspect
+      puts @response.parsed_body
+      puts @response.body
+      puts @response.header
+
+      expect(@response.status).to eq(200)
+
+      # expect(JSON.parse(response.body).length).to eq(1)
+      # expect(@listings.length).to eq(1)
+      # expect(response).to render_template("application")
+      # puts response.inspect
+      # puts JSON.parse(@response.body)
+
+      # expect(response.body.listings).to include(@listing)
     end
   end
 end
